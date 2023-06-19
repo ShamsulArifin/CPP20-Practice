a067dac5c63da8e7aa95712a7300e01500ce5d98
Shamsul Arifin <arifinbd6@gmail.com>
2023-06-19 21:46:36 +0600

    added Booleans

--- Booleans/main.cpp
+++ Booleans/main.cpp
@@ -13,5 +13,29 @@ int main()
   {
     std::cout << "Go through!" << std::endl;
   }
+
+  if (greenLight)
+  {
+    std::cout << "The light is green!" << std::endl;
+  }
+  else
+  {
+    std::cout << "The light is not green!" << std::endl;
+  }
+
+  // sizeof()
+  std::cout << "sizeof(bool): " << sizeof(bool) << std::endl;
+
+  // printing out a bool
+  // 1 -->> true
+  // 0 -->> false
+  std::cout << std::endl;
+  std::cout << "red light: " << redLight << std::endl;
+  std::cout << "green light: " << greenLight << std::endl;
+
+  std::cout << std::boolalpha;
+  std::cout << "red light: " << redLight << std::endl;
+  std::cout << "green light: " << greenLight << std::endl;
+
   return 0;
 }
