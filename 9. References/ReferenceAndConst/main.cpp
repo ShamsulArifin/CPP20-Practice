90600455e82c7cc144ab304438aaa5366212cde4
Shamsul Arifin <arifinbd6@gmail.com>
2023-07-25 22:07:53 +0600

    Added ReferenceAndConst

---
+++ 9. References/ReferenceAndConst/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+  //* Non const reference
+  std::cout << std::endl;
+  std::cout << "Non const reference: " << std::endl;
+  int age{27};
+  int &refAge{age};
+  /*
+    std::cout << "age: " << age << std::endl;
+    std::cout << "refAge: " << refAge << std::endl;
+
+    //* can modify original variable through reference
+    std::cout << std::endl;
+    std::cout << "Modify original variable through reference: " << std::endl;
+
+    refAge++; //* modify through reference
+
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "refAge: " << refAge << std::endl;
+  */
+
+  //* simulating reference behaviour with pointer
+  //! compiler error
+  // const int *const pAge {&age};
+
+  // *pAge = 45;
+
+  return 0;
+}
